d15e5380c6598c21755c1030f7f9128eb528b987
Igor Rybakov <codevanger@gmail.com>
2023-01-06 17:17:20 +0200

    Deleted reduntant types

--- src/utils/types/listener.d.ts
+++
@@ -1,8 +0,0 @@
-import { EmitData } from "./emit-data.d.ts";
-
-export declare type Listener = (
-  listeners: Listeners,
-  data: EmitData | null
-) => void;
-
-export declare type Listeners = Array<Listener>;
